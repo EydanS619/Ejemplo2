1d902fb9cb660972cbed41366de98c8aaea1dfc9
Dell <Dell@DESKTOP-AHFAMKD>
2023-09-03 19:27:11 -0600

    Escribi un metodo

--- Ejemplo2/Program.cs
+++ Ejemplo2/Program.cs
@@ -5,3 +5,10 @@ Console.WriteLine("Prueba de codigo, un cambio mas");
 Console.WriteLine("Prueba de codigo, un cambio mas");
 Console.WriteLine("Prueba de codigo, un cambio mas");
 Console.WriteLine("Prueba de codigo, un cambio mas");
+
+MiMetodo();
+
+void MiMetodo()
+{
+    Console.WriteLine("Ahora tengo un metodo");
+}
